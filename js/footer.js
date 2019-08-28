9a67b1963a3c1efeda5860348f211bf0fc687dee
AdamC19 <cordingleyadam19+github@gmail.com>
2019-08-28 11:55:32 -0400

    Fix image src path issue on blog/index.html

---
+++ js/footer.js
@@ -0,0 +1,39 @@
+document.write(
+  "<div id='footer'>\
+    <ul>\
+      <li id='github'>\
+        <a href='https://github.com/cwruRobotics' target=\"_blank\">\
+          <img alt='GitHub' src='res/footer/github-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='facebook'>\
+        <a href='https://www.facebook.com/cwrurobotics' target=\"_blank\">\
+          <img alt='Facebook' src='res/footer/facebook-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='instagram'>\
+        <a href= 'https://www.instagram.com/cwrubotix/' target=\"_blank\">\
+          <img alt='Instagram' src='res/footer/instagram-icon.jpg' />\
+        </a>\
+      </li>\
+      <li id='twitter'>\
+        <a href='https://twitter.com/cwrubotix' target=\"_blank\">\
+          <img alt='Twitter' src='res/footer/twitter-logo.svg' />\
+        </a>\
+      </li>\
+      <li id='youtube'>\
+        <a href='https://www.youtube.com/channel/UCFxYzjLEY09YFLx_UKSINIw' target=\"_blank\">\
+          <img alt='YouTube' src='res/footer/youtube-icon.svg' />\
+        </a>\
+      </li>\
+	    <li id='email'>\
+        <a href='mailto:robotics-exec@case.edu'>\
+          <img alt='Email' src='res/footer/email-icon.svg' />\
+        </a>\
+      </li>\
+    </ul>\
+    <p id='copyright'>\
+      Copyright © 2017 CWRUbotix\
+    </p>\
+  </div>"
+);
---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+document.write(
+  "<ul id='nav'>\
+    <img id='nav_logo' src='res/logo.svg'/>\
+    <div class='nav_title'>\
+      <p id='nav_name'><b>CWRUbotix</b></p>\
+      <p id='nav_desc'>Case Western Reserve University's Robotics Team</p>\
+    </div>\
+    <li><a href='index.html#main'>Home</a></li>\
+    <li><a href='index.html#team'>Members</a></li>\
+	  <li><a href='index.html#projects'>Projects</a></li>\
+    <li><a href='blog/index.html'>Blog</a></li>\
+	  <li><a href='index.html#sponsorship'>Sponsorship</a></li>\
+    <li><a href='index.html#contact'>Contact</a></li>\
+  </ul>"
+);
+
+window.onscroll = function() {f_scroll()};
+
+function f_scroll() {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav_logo").style.maxWidth = "80px";
+		document.getElementById("nav_name").style.fontSize = "26px";
+		document.getElementById("nav_desc").style.fontSize = "0px";
+    } else {
+        document.getElementById("nav_logo").style.maxWidth = "120px";
+		document.getElementById("nav_name").style.fontSize = "30px";
+		document.getElementById("nav_desc").style.fontSize = "16px";
+    }
+}
